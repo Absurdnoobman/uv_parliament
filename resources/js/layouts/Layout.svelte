9ff8cbf72173ef2793f40947617711cac96bda30
LadsForTea <88262542+Absurdnoobman@users.noreply.github.com>
2026-08-24 19:13:13 +0700

    Second Commit

---
+++ resources/js/layouts/Layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+    import AppHead from "@/components/AppHead.svelte";
+
+    let { children }: {children: Snippet | null | undefined } = $props()
+</script>
+
+<AppHead>
+    <!-- <link rel="preconnect" href="https://rsms.me/" />
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css" /> -->
+</AppHead>
+
+<div>
+    <div class="flex flex-row gap-1 py-4 px-6 shadow-sm">
+        <img src="/assets/coat_of_arm_no_bg.webp" alt="UV Coat of Arm" class=" aspect-square w-14 mr-4" />
+        <div class="content-center"><div class="text-2xl font-bold">UV Parliament</div></div>
+    </div>
+    <div>
+        {@render children?.()}
+    </div>
+    <footer class="bg-black text-white px-10">
+        Footer
+    </footer>
+</div>
